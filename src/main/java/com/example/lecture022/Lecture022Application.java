32a77be01ecab050be0ef0b07110d54972952c76
thermoCat <jangsangmin0401@gmail.com>
2025-01-17 11:12:15 +0900

    First commit

---
+++ src/main/java/com/example/lecture022/Lecture022Application.java
@@ -0,0 +1,13 @@
+package com.example.lecture022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lecture022Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lecture022Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lecture022/Lecture022ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lecture022;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lecture022ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
